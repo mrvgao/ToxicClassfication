cf54f68f911ae8138f59df3b3d086414e54d40df
fortymiles <mqgao@outlook.com>
2018-01-19 14:47:24 +0800

    get all corpus into a line

---
+++ get_all_corpus.py
@@ -0,0 +1,15 @@
+from itertools import chain
+import pandas as pd
+from utilities import format_string
+import config
+
+test_content = pd.read_csv('data/test.csv')
+original_content = pd.read_csv('data/train.csv')
+
+with open('{}/line_corpus.txt'.format(config.root), 'w') as f:
+    for row in chain(*[df.iterrows() for df in [test_content, original_content]]):
+        try:
+            sentence = format_string(str(row[1].comment_text))
+            f.write(sentence + '\n')
+        except TypeError:
+            continue

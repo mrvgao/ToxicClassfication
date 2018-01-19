23da5fc6d6a16d47a8baabe7d735b2ba8d822b3b
fortymiles <mqgao@outlook.com>
2018-01-19 15:59:07 +0800

    change windows

--- grid_search.py
+++ grid_search.py
@@ -23,13 +23,17 @@ def train(label, model, dim, lr, windows, epoch, record, thread):
     clf_path = '{}/clf/{}-{}-{}-{}-{}_model'.format(config.root, model, dim, lr, windows, epoch)
 
     train_file, dev_file = get_train_dev_corpus_file_name(label=label)
-    classifier = fasttext.supervised(
-        train_file,
-        clf_path,
-        dim=dim,
-        pretrained_vectors=w2v_model_path,
-        thread=threads
-    )
+
+    if not os.path.exists(clf_path + '.bin'):
+        classifier = fasttext.supervised(
+            train_file,
+            clf_path,
+            dim=dim,
+            pretrained_vectors=w2v_model_path,
+            thread=threads
+        )
+    else:
+        classifier = fasttext.load_model(clf_path + '.bin')
 
     print('WHEN LABEL = {} DIM = {}, LR = {}, windows = {}, epoch = {}, model = {}'.format(label, dim, lr, windows, epoch, model))
     result = classifier.test(dev_file)
@@ -43,7 +47,7 @@ if __name__ == '__main__':
     models = ['cbow', 'skip']
     dimensons = [20, 50, 100, 200]
     learning_rates = [1e-3, 1e-2]
-    ws = [3, 5, 7]
+    ws = [5, 7, 9]
     epochs = [5, 10, 15]
     threads = 50
 

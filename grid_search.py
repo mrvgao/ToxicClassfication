32a0c7079922540f9eef27fd3acb65910b9fa9fd
fortymiles <mqgao@outlook.com>
2018-01-19 16:03:54 +0800

    change ws

--- grid_search.py
+++ grid_search.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     models = ['skip', 'cbow']
     dimensons = [30, 60, 100]
     learning_rates = [1e-3, 1e-2]
-    ws = [3, 5, 7]
+    ws = [3, 6, 9]
     epochs = [5, 10, 15]
     threads = 50
 

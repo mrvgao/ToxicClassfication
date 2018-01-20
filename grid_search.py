d2407d1e8e8cdd460194ef86e15d894c369c0bc8
fortymiles <mqgao@outlook.com>
2018-01-20 13:59:40 +0800

    fix del typo

--- grid_search.py
+++ grid_search.py
@@ -37,7 +37,7 @@ def train_and_predicate(label, model, dim, lr, windows, epoch):
     result = classifier.test(dev_file)
     print(" PRECISION: {}, RECALL: {}".format(result.precision, result.recall))
 
-    del fasttext
+    del classifier
 
     # with open(record, 'a') as f:
     return '{}-{}-{}-{}-{}-{}-precision-{}-recall-{}\n'.format(label, model, dim, lr, windows, epoch, result.precision, result.recall)

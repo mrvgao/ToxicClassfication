fcd6ffa3b3a66bdfeb22b40312f5c19a666e7633
fortymiles <mqgao@outlook.com>
2018-01-20 23:09:25 +0800

    reget train corpus

--- get_all_corpus.py
+++ get_all_corpus.py
@@ -3,11 +3,12 @@ import pandas as pd
 from utilities import format_string
 import config
 
-test_content = pd.read_csv('data/test.csv')
-original_content = pd.read_csv('data/train.csv')
+files = ['data/test.csv', 'data/train.csv', 'data/train_old.csv', 'data/test_old.csv']
+
+contents = [pd.read_csv(f) for f in files]
 
 with open(config.line_corpus, 'w') as f:
-    for row in chain(*[df.iterrows() for df in [test_content, original_content]]):
+    for row in chain(*[df.iterrows() for df in contents]):
         try:
             sentence = format_string(str(row[1].comment_text))
             f.write(sentence + '\n')

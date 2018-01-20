00947714c2206dc529224a985acd53f6703dc07d
fortymiles <mqgao@outlook.com>
2018-01-20 11:28:28 +0800

    change hyper paramters

--- paramters.py
+++ paramters.py
@@ -1,8 +1,12 @@
 class Parameters:
     models = ['skip', 'cbow']
-    dimensons = [30, 60, 100]
-    learning_rates = [1e-3, 1e-2]
-    ws = [3, 6, 9]
-    epochs = [5, 10, 15]
+    # dimensons = [30, 60, 100]
+    dimensons = [30, ]
+    # learning_rates = [1e-3, 1e-2]
+    learning_rates = [1e-3, ]
+    # ws = [3, 6, 9]
+    ws = [3, ]
+    # epochs = [5, 10, 15]
+    epochs = [5, 10, ]
     threads = 50
 

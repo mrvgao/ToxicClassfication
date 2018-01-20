29847806f8cfca944e64961149e48c15a4b5cdb0
fortymiles <mqgao@outlook.com>
2018-01-20 23:19:39 +0800

    fix tyop

--- format_test_data.py
+++ format_test_data.py
@@ -3,7 +3,7 @@ import csv
 import pandas as pd
 
 with open('data/new_test.csv', 'w', encoding='utf-8') as f:
-    writer = csv.writer()
+    writer = csv.writer(f)
     writer.writerow(['id', 'comment_text'])
     for row in pd.read_csv('data/test.csv').iterrows():
         _id, content = row[1]['id'], row[1]['comment_text']

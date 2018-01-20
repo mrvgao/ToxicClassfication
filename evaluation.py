c3bf70018a57c6a9fcaf7ba8dd8a7d6a84ce0431
fortymiles <mqgao@outlook.com>
2018-01-20 15:08:39 +0800

    add evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,5 @@
 def evaluation(clf, dev_file):
-    def_text = [' '.join(line.split()[1:]) for line in open(dev_file)]
+    def_text = [' '.join(line.split()[1:]) for line in open(dev_file, encoding='utf-8')]
     labels = clf.predict(def_text)
     print(labels)
 

be378c046bbe44074a6bc5937d2092899b54e989
fortymiles <mqgao@outlook.com>
2018-01-20 23:12:37 +0800

    add encoding

--- get_all_corpus.py
+++ get_all_corpus.py
@@ -7,7 +7,7 @@ files = ['data/test.csv', 'data/train.csv', 'data/train_old.csv', 'data/test_old
 
 contents = [pd.read_csv(f) for f in files]
 
-with open(config.line_corpus, 'w') as f:
+with open(config.line_corpus, 'w', encoding='utf-8') as f:
     for row in chain(*[df.iterrows() for df in contents]):
         try:
             sentence = format_string(str(row[1].comment_text))

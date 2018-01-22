cc872735407fa268b5ec44fe74dd02ae525e2165
fortymiles <mqgao@outlook.com>
2018-01-22 20:56:06 +0800

    change to small paramters confornt overfitting

--- paramters.py
+++ paramters.py
@@ -1,12 +1,12 @@
 class Parameters:
     models = ['skip', 'cbow']
-    dimensons = [30, 60, 100]
+    dimensons = [30]
     # dimensons = [30, ]
-    learning_rates = [1e-3, 1e-2]
+    learning_rates = [1e-3]
     # learning_rates = [1e-3, ]
-    ws = [3, 6, 9]
+    ws = [9]
     # ws = [3, ]
-    epochs = [5, 10, 15]
+    epochs = [15]
     # epochs = [5, 10, ]
     threads = 50
 

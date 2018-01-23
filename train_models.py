c2415cc66488aa968c7e2b838ba90e750e7c810b
fortymiles <mqgao@outlook.com>
2018-01-23 16:46:22 +0800

    change ngram to 5

--- train_models.py
+++ train_models.py
@@ -31,7 +31,7 @@ def train(label, model, dim, lr, windows, epoch, first_train=False):
             dim=dim,
             # pretrained_vectors=w2v_model_path,
             thread=80,
-            word_ngrams=3,
+            word_ngrams=5,
             bucket=2000000,
     )
 

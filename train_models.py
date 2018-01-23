240166b8b359bbfba42ebde16407e4dcf30f4680
fortymiles <mqgao@outlook.com>
2018-01-23 16:27:25 +0800

    add bucket

--- train_models.py
+++ train_models.py
@@ -31,7 +31,8 @@ def train(label, model, dim, lr, windows, epoch, first_train=False):
             dim=dim,
             # pretrained_vectors=w2v_model_path,
             thread=80,
-            word_ngrams=2,
+            word_ngrams=3,
+            bucket=2000000,
     )
 
 

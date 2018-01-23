4e5177ae07da16c7a81661f3f3e90f15e4856909
fortymiles <mqgao@outlook.com>
2018-01-23 12:38:44 +0800

    fix remove all files in a dicectory

--- utilities.py
+++ utilities.py
@@ -8,5 +8,6 @@ def format_string(string):
 
 
 def remove_folder(folder):
-    shutil.rmtree(folder)
-    os.mkdir(folder)
+    for file in os.listdir(folder):
+        os.remove(os.path.join(folder, file))
+        print('remove file {}'.format(os.path.join(folder, file)))

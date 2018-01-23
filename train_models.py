525ad12a6ae042a30e23c9d1343450af01b439a7
fortymiles <mqgao@outlook.com>
2018-01-23 16:14:48 +0800

    remove loss

--- train_models.py
+++ train_models.py
@@ -32,7 +32,6 @@ def train(label, model, dim, lr, windows, epoch, first_train=False):
             pretrained_vectors=w2v_model_path,
             thread=80,
             word_ngrams=2,
-            loss='hs',
     )
 
 

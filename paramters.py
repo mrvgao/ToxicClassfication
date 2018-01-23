067dbd454d8c8d97524c5be79182993482d1f761
fortymiles <mqgao@outlook.com>
2018-01-23 11:05:41 +0800

    only one condition

--- paramters.py
+++ paramters.py
@@ -1,12 +1,8 @@
 class Parameters:
-    models = ['skip', 'cbow']
+    models = ['cbow']
     dimensons = [30]
-    # dimensons = [30, ]
     learning_rates = [1e-3]
-    # learning_rates = [1e-3, ]
-    ws = [9]
-    # ws = [3, ]
-    epochs = [15]
-    # epochs = [5, 10, ]
+    ws = [6]
+    epochs = [5]
     threads = 50
 

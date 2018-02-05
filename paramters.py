ff9b8bd2411c8606082fd7841f2c5a75af267334
fortymiles <mqgao@outlook.com>
2018-02-05 19:07:21 +0800

    change parameters

--- paramters.py
+++ paramters.py
@@ -1,8 +1,8 @@
 class Parameters:
     models = ['cbow']
-    dimensons = [128]
+    dimensons = [300]
     learning_rates = [1e-2]
-    ws = [3]
+    ws = [5]
     epochs = [10]
     threads = 50
 
